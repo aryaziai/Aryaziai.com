ccf4fb43c2618d96c48b50399a832c88230dcfbf
aryaziai <arya721@gmail.com>
2020-04-09 15:59:29 -0700

    .

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d1ebe6869ff5b32957e91229ed7a5b85.js"
+  "/precache-manifest.89be521e254556a1df7160908140efbd.js"
 );
 
 self.addEventListener('message', (event) => {
