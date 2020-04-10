4145a51974b3215cbb67fa578a3f13ae059a16f1
aryaziai <arya721@gmail.com>
2020-04-09 17:37:06 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+
 import Intro from "./components/Intro.js";
 import About from "./components/About.js";
 import Resume from "./components/Resume.js";
